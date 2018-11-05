692938260b8c8daaf25947c9275d90c180952f90
User12 <user12@user12de-imac.local>
2018-11-05 13:36:41 +0800

    Initial Commit

---
+++ new2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new2
+//
+//  Created by User12 on 2018/11/5.
+//  Copyright © 2018 User12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
